5bc5927d646b1bb0291183ff89a56113b29f2388
Filippo Taiani <FilippoTaiani05@gmail.com>
2026-05-28 13:26:50 +0200

    Implemented natural behaviour for producers

--- src/network.c
+++ src/network.c
@@ -6,18 +6,21 @@
 #include <sys/socket.h>
 #include "network.h"
 
+#define COLOR_RED   "\033[0;31m"
+#define COLOR_RESET "\033[0m"
+
 // Create server socket, apply SO_REUSEADDR and bind+listen
 int setup_server_socket(int port, int maxconnections) {
     int server_fd  = socket(AF_INET, SOCK_STREAM, 0);
     if(server_fd == -1){
-        perror("Error while creating socket");
+        perror(COLOR_RED "[NETWORK] Error while creating socket" COLOR_RESET);
         exit(EXIT_FAILURE);
     }
     
     // rapid reuse of IP:PORT via REUSEADDR
     int opt = 1;
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) {
-        perror("Error in setsockopt");
+        perror(COLOR_RED "[NETWORK] Error in setsockopt" COLOR_RESET);
         exit(EXIT_FAILURE);
     }
 
@@ -27,12 +30,12 @@ int setup_server_socket(int port, int maxconnections) {
     address.sin_port = htons(port);
 
     if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("Error while attempting bind");
+        perror(COLOR_RED "[NETWORK] Error while attempting bind between socket %d and address %s" COLOR_RESET, server_fd, inet_ntoa(address.sin_addr));
         exit(EXIT_FAILURE);
     }
 
     if (listen(server_fd, 10) < 0) {
-        perror("Errore while attempting listen");
+        perror(COLOR_RED "[NETWORK] Error while attempting listen on socket %d" COLOR_RESET, server_fd);
         exit(EXIT_FAILURE);
     }
 
@@ -42,8 +45,8 @@ int setup_server_socket(int port, int maxconnections) {
 int connect_to_server(const char* ip, int port) {
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
-        perror("Error creating socket for client");
-        exit(EXIT_FAILURE);
+        perror(COLOR_RED "[NETWORK] Error creating socket for client" COLOR_RESET);
+        return -1;
     }
 
     struct sockaddr_in serv_addr;
@@ -51,13 +54,15 @@ int connect_to_server(const char* ip, int port) {
     serv_addr.sin_port = htons(port);
 
     if (inet_pton(AF_INET, ip, &serv_addr.sin_addr) <= 0) {
-        perror("Address not valid or not supported");
-        exit(EXIT_FAILURE);
+        perror(COLOR_RED "[NETWORK] Address not valid or not supported" COLOR_RESET);
+        close(sock);
+        return -1;
     }
 
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        perror("Connection failed");
-        exit(EXIT_FAILURE);
+        perror(COLOR_RED "[NETWORK] Error connecting to server" COLOR_RESET);
+        close(sock);
+        return -1;
     }
     return sock;
 }
--- src/producer.c
+++ src/producer.c
@@ -16,22 +16,43 @@ int main() {
 
     printf(COLOR_GRAY "[Producer %d] Starting producer...\n" COLOR_RESET, getpid());
 
-    int sock = connect_to_server(SERVER_IP, SERVER_PORT);
-    printf(COLOR_GREEN "[Producer %d] Succesfully connected to coordinator.\n" COLOR_RESET, getpid());
+    int sock = -1;
 
-    // Test message
-    LogMessage msg;
-    msg.sender_id = getpid();
-    msg.data = ((double)rand() / RAND_MAX) * 1000.0; // random (0.00 -> 1000.00)
-
-    // Test send
-    if (send(sock, &msg, sizeof(LogMessage), 0) < 0) {
-        perror(COLOR_RED "[Producer %d] Error whilst sending message" COLOR_RESET, getpid());
-        exit(EXIT_FAILURE);
+    while (sock < 0) { // keep trying to connect until successful
+        sock = connect_to_server(SERVER_IP, SERVER_PORT);
+        if (sock < 0) {
+            printf(COLOR_ORANGE "[Producer %d] Connection failed/refused. Retrying in 1s...\n" COLOR_RESET, getpid());
+            sleep(1);
+        }
     }
 
-    printf(COLOR_GRAY "[Producer %d] Message sent (ID: %d, Data: %.2f)!\n" COLOR_RESET, getpid(), msg.sender_id, msg.data);
+    printf(COLOR_GREEN "[Producer %d] Succesfully connected to coordinator.\n" COLOR_RESET, getpid());
 
+    // Random number of messages to send (1-5)
+    int num_messages = (rand() % 5) + 1;
+
+    for (int i = 0; i < num_messages; i++) {
+        LogMessage msg;
+        msg.sender_id = getpid();
+        msg.data = ((double)rand() / RAND_MAX) * 1000.0; // random (0.00 -> 1000.00)
+
+        // try to send
+        if (send(sock, &msg, sizeof(LogMessage), 0) < 0) {
+            perror(COLOR_RED "[Producer %d] Error whilst sending message" COLOR_RESET, getpid());
+            break; // if connection blows up, stop trying to send more messages
+        }
+
+        printf(COLOR_GRAY "[Producer %d] Message %d/%d sent (Data: %.2f)!\n" COLOR_RESET, getpid(), i+1, num_messages, msg.data);
+
+        // random delay between 500ms and 1500ms before sending the next message
+        // (only if there are more messages to send, otherwise we can just disconnect immediately)
+        if (i < num_messages - 1) {
+            int delay_ms = 500 + (rand() % 1000); // 500 + (0 to 999)
+            usleep(delay_ms * 1000); // usleep uses microseconds, so conversion is needed
+        }
+    }
+
+    printf(COLOR_GRAY "[Producer %d] Finished sending data. Disconnecting.\n" COLOR_RESET, getpid());
     close(sock);
     return 0;
 }
